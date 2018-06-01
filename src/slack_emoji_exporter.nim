c3f5fb7f89965a9d434a08ce31f0387ad9c1a54b
Anthony Scotti <anthony.m.scotti@gmail.com>
2018-06-01 16:18:58 -0400

    Fix formatting

--- src/slack_emoji_exporter.nim
+++ src/slack_emoji_exporter.nim
@@ -1,35 +1,36 @@
-import httpclient, json, os, ospaths, strformat, strutils, times, uri
+import
+  httpclient, json, os, ospaths, strformat, strutils, times, uri
 
-const 
-    SlackApiTokenEnvironmentVariable = "SLACK_API_TOKEN"
-    SlackApiEmojiEndpoint = "https://slack.com/api/emoji.list"
-    TimestampFormat = "yyyyMMddhhmmss"
+const
+  SlackApiTokenEnvironmentVariable = "SLACK_API_TOKEN"
+  SlackApiEmojiEndpoint = "https://slack.com/api/emoji.list"
+  TimestampFormat = "yyyyMMddhhmmss"
 
 proc downloadImages(token: string) =
-    let client = newHttpClient()
+  let client = newHttpClient()
     
-    client.headers = newHttpHeaders([
-        ("Content-Type", "application/json"),
-        ("Authorization", fmt"Bearer {token}")
-    ])
-    let emojiData = client.getContent(SlackApiEmojiEndpoint).parseJson()
+  client.headers = newHttpHeaders([
+    ("Content-Type", "application/json"),
+    ("Authorization", fmt"Bearer {token}")
+  ])
+  let emojiData = client.getContent(SlackApiEmojiEndpoint).parseJson()
 
-    let dateFolder = local(getTime()).format(TimestampFormat)
+  let dateFolder = local(getTime()).format(TimestampFormat)
 
-    discard existsOrCreateDir(dateFolder)
+  discard existsOrCreateDir(dateFolder)
 
-    for item in emojiData["emoji"].pairs:
-        let imageUrl = item.val.getStr
-        if not imageUrl.contains("alias:"):
-            let imageExt = imageUrl.splitPath().tail.splitFile().ext
-            echo fmt"Downloading '{item.key}' as {item.key & imageExt}"
-            client.downloadFile(imageUrl, joinPath(dateFolder, item.key & imageExt))
+  for item in emojiData["emoji"].pairs:
+    let imageUrl = item.val.getStr
+    if not imageUrl.contains("alias:"):
+      let imageExt = imageUrl.splitPath().tail.splitFile().ext
+      echo fmt"Downloading '{item.key}' as {item.key & imageExt}"
+      client.downloadFile(imageUrl, joinPath(dateFolder, item.key & imageExt))
 
 proc main() =
-    if existsEnv(SlackApiTokenEnvironmentVariable):
-        downloadImages(getEnv(SlackApiTokenEnvironmentVariable))
-    else:
-        echo fmt"Environment variable {SlackApiTokenEnvironmentVariable} isn't set"
+  if existsEnv(SlackApiTokenEnvironmentVariable):
+    downloadImages(getEnv(SlackApiTokenEnvironmentVariable))
+  else:
+    echo fmt"Environment variable {SlackApiTokenEnvironmentVariable} isn't set"
 
 if isMainModule:
-    main()
+  main()
